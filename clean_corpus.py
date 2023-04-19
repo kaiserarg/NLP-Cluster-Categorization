b20dcd9d8586d395b5d82fc3cfeea740033c165b
dq2024 <99848111+dq2024@users.noreply.github.com>
2023-04-18 23:53:14 -0400

    clean_corpus.py

---
+++ clean_corpus.py
@@ -0,0 +1,9 @@
+import nltk
+import string
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('stopwords')

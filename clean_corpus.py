1f2ee8cb2029a55d3a33187a273269b2be0c7fbe
dq2024 <99848111+dq2024@users.noreply.github.com>
2023-04-19 00:39:56 -0400

    update clean_corpus.py

--- clean_corpus.py
+++ clean_corpus.py
@@ -7,3 +7,19 @@ from nltk.corpus import stopwords
 nltk.download('punkt')
 nltk.download('wordnet')
 nltk.download('stopwords')
+
+def preprocess_corpus(corpus):
+    tokens = word_tokenize(corpus)
+    tokens = [word.lower() for word in tokens]
+
+    # Remove punctuation from tokens
+    tokens = [word for word in tokens if word not in string.punctuation]
+    lemmatizer = WordNetLemmatizer()
+
+    # Lemmatize tokens to their base form (singularize)
+    tokens = [lemmatizer.lemmatize(word) for word in tokens]
+
+    return tokens
+
+with open("clean_corpus.py", "r") as file:
+    tokens = preprocess_corpus(file)

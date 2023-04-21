1435c429ffbfa39c62d0ad6b18400303e582063e
dq2024 <99848111+dq2024@users.noreply.github.com>
2023-04-20 20:09:37 -0400

    Summary (required)

--- scrape.py
+++ scrape.py
@@ -2,6 +2,31 @@ from youtube_transcript_api import YouTubeTranscriptApi as transcriptAPI
 from youtube_transcript_api.formatters import TextFormatter
 import scrapetube
 import json
+import nltk
+import string
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('stopwords')
+
+def clean_corpus(corpus):
+    tokens = word_tokenize(corpus)
+    tokens = [word.lower() for word in tokens]
+   
+    tokens = [word for word in tokens if word not in string.punctuation]
+    tokens = [word for word in tokens if not any(char in string.punctuation for char in word)]
+    tokens = [word for word in tokens if word not in ('”', '“', '’', '‘')]
+    stop_words = set(stopwords.words('english'))
+    tokens = [word for word in tokens if word.lower() not in stop_words]
+    lemmatizer = WordNetLemmatizer()
+
+    tokens = [lemmatizer.lemmatize(word) for word in tokens]
+    string_of_tokens = ' '.join(tokens)
+
+    return string_of_tokens
 
 formatter = TextFormatter()
 
@@ -23,6 +48,7 @@ for youtuber in youtuberList:
             continue
     
     #cleaning goes here
+    corpus = clean_corpus(corpus)
     jsonDict[youtuber] = {"name": youtuber, "corpus": corpus}
 
 json.dump(jsonDict, f, indent=3)

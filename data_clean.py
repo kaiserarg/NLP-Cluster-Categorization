1a477def8d3116bbf33ce0509f8803b0305957ae
dq2024 <99848111+dq2024@users.noreply.github.com>
2023-04-22 18:07:24 -0400

    deniz commit

--- data_clean.py
+++ data_clean.py
@@ -1,29 +1,52 @@
 import nltk
 import string
+from stop_list import closed_class_stop_words
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-from youtuberlist import youtuberList
 from nltk import re
+import json
 
-def clean_corpus(corpus):
+
+f= open("cleanedData.json", "r")
+
+data = json.load(f)
+cleaned_data = {}
+
+for youtuber in data:
+    corpus = data[youtuber]["corpus"]
+
     tokens = word_tokenize(corpus)
     tokens = [word.lower() for word in tokens]
-    
-    #remove words in corpus with parenthesis around them, in the youtube transcript this would filter out (laughs) or (music playing)
-    tokens = [re.sub(r'\([^)]*\)', '', word) for word in tokens]
+
+    #remove stop words from assignment4
+    tokens = [word for word in tokens if word not in closed_class_stop_words]
+    #remove things (laughter)
+    tokens = [word for word in tokens if not any(char in '(' for char in word)] 
     #remove punctuation
     tokens = [word for word in tokens if word not in string.punctuation]
+
+    #remove common exclamations
+    tokens = [re.sub(r'(oh.*)|(hah.*)|(ah.*)|(ye.*)|(uh.*)|(hm.*)|(sh.*)|(um.*)', '', word) for word in tokens] 
+
     #remove words like 's --> for some reason the tokenizer splits up words like dog's into dog and 's 
-    tokens = [re.sub(r'(\([^)]*\))|(\\u266a)', '', word) for word in tokens] #remove things (laughter) and ♪
-    tokens = [word for word in tokens if word not in string.punctuation] 
-    tokens = [word for word in tokens if not any(char in string.punctuation for char in word)]
-    tokens = [word for word in tokens if word not in ('”', '“', '’', '‘')]
+    tokens = [word for word in tokens if not any(char in (string.punctuation) for char in word)]
+
+    tokens = [word for word in tokens if word not in ('”', '“', '’', '‘', 'yes', 'no', 'ugh', 'really', 'eh', 'gon', 'na', 'wow', 'wan', 
+                                                      'na', 'hey', 'phew', 'u', 'le', 'ca', 'la', 'bl', 'hum')]
+    
     stop_words = set(stopwords.words('english'))
     tokens = [word for word in tokens if word.lower() not in stop_words]
+    
     lemmatizer = WordNetLemmatizer()
-
     tokens = [lemmatizer.lemmatize(word) for word in tokens]
+    
     string_of_tokens = ' '.join(tokens)
+    #remove all unicode characters like "\u00e9", "\u2026", "\u00fc"
+    string_of_tokens = ''.join(char for char in string_of_tokens if ord(char) < 128)
 
-    return string_of_tokens
+    cleaned_data[youtuber] = {"corpus": string_of_tokens}
+
+with open("new_clean_data.json", "w") as output:
+    json.dump(cleaned_data, output, indent=1)
+    
---
+++ stop_list.py
@@ -0,0 +1,24 @@
+
+closed_class_stop_words = ['a','the','an','and','or','but','about','above','after','along','amid','among',\
+                           'as','at','by','for','from','in','into','like','minus','near','of','off','on',\
+                           'onto','out','over','past','per','plus','since','till','to','under','until','up',\
+                           'via','vs','with','that','can','cannot','could','may','might','must',\
+                           'need','ought','shall','should','will','would','have','had','has','having','be',\
+                           'is','am','are','was','were','being','been','get','gets','got','gotten',\
+                           'getting','seem','seeming','seems','seemed',\
+                           'enough', 'both', 'all', 'your' 'those', 'this', 'these', \
+                           'their', 'the', 'that', 'some', 'our', 'no', 'neither', 'my',\
+                           'its', 'his' 'her', 'every', 'either', 'each', 'any', 'another',\
+                           'an', 'a', 'just', 'mere', 'such', 'merely' 'right', 'no', 'not',\
+                           'only', 'sheer', 'even', 'especially', 'namely', 'as', 'more',\
+                           'most', 'less' 'least', 'so', 'enough', 'too', 'pretty', 'quite',\
+                           'rather', 'somewhat', 'sufficiently' 'same', 'different', 'such',\
+                           'when', 'why', 'where', 'how', 'what', 'who', 'whom', 'which',\
+                           'whether', 'why', 'whose', 'if', 'anybody', 'anyone', 'anyplace', \
+                           'anything', 'anytime' 'anywhere', 'everybody', 'everyday',\
+                           'everyone', 'everyplace', 'everything' 'everywhere', 'whatever',\
+                           'whenever', 'whereever', 'whichever', 'whoever', 'whomever' 'he',\
+                           'him', 'his', 'her', 'she', 'it', 'they', 'them', 'its', 'their','theirs',\
+                           'you','your','yours','me','my','mine','I','we','us','much','and/or'
+                           ]
+

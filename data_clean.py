0cdb021c38922ad9ede1ef6ffbb0f3d7884926a9
dq2024 <99848111+dq2024@users.noreply.github.com>
2023-04-22 14:54:05 -0400

    Update data_clean.py

--- data_clean.py
+++ data_clean.py
@@ -9,9 +9,12 @@ from nltk import re
 def clean_corpus(corpus):
     tokens = word_tokenize(corpus)
     tokens = [word.lower() for word in tokens]
-   
+    
+    #remove words in corpus with parenthesis around them, in the youtube transcript this would filter out (laughs) or (music playing)
     tokens = [re.sub(r'\([^)]*\)', '', word) for word in tokens]
+    #remove punctuation
     tokens = [word for word in tokens if word not in string.punctuation]
+    #remove words like 's --> for some reason the tokenizer splits up words like dog's into dog and 's 
     tokens = [word for word in tokens if not any(char in string.punctuation for char in word)]
     tokens = [word for word in tokens if word not in ('”', '“', '’', '‘')]
     stop_words = set(stopwords.words('english'))

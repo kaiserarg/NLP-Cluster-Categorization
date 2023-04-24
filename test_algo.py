45ded93081158c454ac410cd06611ba9ae6160aa
ShenyiHuang <moucangcang@gmail.com>
2023-04-24 13:53:18 -0400

    Update test_algo.py

--- test_algo.py
+++ test_algo.py
@@ -2,22 +2,22 @@ import math
 import numpy as np
 from numpy.linalg import norm
 
-def cossim(m):
-    lst = []
-    for i in range(len(m)):
-        for j in range(i,len(m)):
-            a = np.array(m[i])
-            b = np.array(m[j])
-            lst.append(np.dot(a,b)/(norm(a)*norm(b)))
+# def cossim(m):
+#     lst = []
+#     for i in range(len(m)):
+#         for j in range(i,len(m)):
+#             a = np.array(m[i])
+#             b = np.array(m[j])
+#             lst.append(np.dot(a,b)/(norm(a)*norm(b)))
     
-    m_t = np.array(m).transpose()
-    for i in range(len(m)):
-        for j in range(i,len(m)):
-            a = np.array(m[i])
-            b = np.array(m[j])
-            lst.append(np.dot(a,b)/(norm(a)*norm(b)))
+#     m_t = np.array(m).transpose()
+#     for i in range(len(m)):
+#         for j in range(i,len(m)):
+#             a = np.array(m[i])
+#             b = np.array(m[j])
+#             lst.append(np.dot(a,b)/(norm(a)*norm(b)))
     
-    return sum(lst) / len(lst)
+#     return sum(lst) / len(lst)
 
 
 def score(m):
@@ -25,10 +25,11 @@ def score(m):
 
 
 ma = [[0.9, 0.9], [1,1]]
-print(cossim(ma))
 print(score(ma))
 mb = [[0.4, 0.4], [0.5,0.5]]
-print(cossim(mb))
 print(score(mb))
+
+
+
 
 

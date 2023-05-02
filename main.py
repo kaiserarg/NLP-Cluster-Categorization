e525b6d5d3f9fe82b9dd4d9eb9794114c09875e6
ShenyiHuang <moucangcang@gmail.com>
2023-05-02 19:27:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from clustering import word2vec_clustering
 
 
-thresholds = [0.7]
+thresholds = [0.6, 0.7, 0.8, 0.9]
 
-# keywords_nums = [5,10,15,20]
-keywords_nums = [10]
+keywords_nums = [5,10,15,20]
 
 for t in thresholds:
     for k in keywords_nums:

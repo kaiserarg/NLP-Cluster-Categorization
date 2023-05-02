a83cc7522137cbdf376d6396219ebf0753830dbc
dq2024 <99848111+dq2024@users.noreply.github.com>
2023-05-02 17:05:46 -0400

    Update keybert

--- keybert_keywords.py
+++ keybert_keywords.py
@@ -3,12 +3,12 @@ from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 import json
 
-def clean(corpus):
-    tokens = word_tokenize(corpus)
-    lemmatizer = WordNetLemmatizer()
-    tokens = [word.lower() for word in tokens]
-    tokens = [lemmatizer.lemmatize(word) for word in tokens]
-    return " ".join(tokens)
+#def clean(corpus):
+#    tokens = word_tokenize(corpus)
+#    lemmatizer = WordNetLemmatizer()
+#    tokens = [word.lower() for word in tokens]
+#    tokens = [lemmatizer.lemmatize(word) for word in tokens]
+#    return " ".join(tokens)
 
 
 kw_model = KeyBERT()
@@ -20,13 +20,12 @@ keywordfile = open("keywords.json", "a")
 count = 0
 data = json.load(f)
 
-keywords = {
-}
+keywords = {}
  
 for youtuber in data:
     if(data[youtuber]["corpus"] == ""):
             continue
-    corpus = clean(data[youtuber]["corpus"])
+    corpus = data[youtuber]["corpus"]
     keyword = kw_model.extract_keywords(corpus, top_n=20)
     keywords[data[youtuber]["name"]] = keyword
     count = count + 1

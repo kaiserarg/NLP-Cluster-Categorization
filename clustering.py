3fb1c532ec375f292ac5df898c0cbeb3f3d87693
ShenyiHuang <moucangcang@gmail.com>
2023-05-02 16:22:28 -0400

    main.py clustering.py

--- clustering.py
+++ clustering.py
@@ -18,7 +18,7 @@ def keyword_similarity(k1, k2, model):
 
     return cosine_similarity(vec1.reshape(1, -1), vec2.reshape(1, -1))[0][0]
 
-def weighted_cosine_similarity(k_lst1, k_lst2, model, threshold=0.85):
+def weighted_cosine_similarity(k_lst1, k_lst2, model, threshold):
     sim_scores = []
 
     for k1 in k_lst1:
@@ -39,41 +39,42 @@ def weighted_cosine_similarity(k_lst1, k_lst2, model, threshold=0.85):
     return score/normalization
 
 
-# load Word2Vec model & get the keywords & scores
-word2vec_model = KeyedVectors.load_word2vec_format("GoogleNews-vectors-negative300.bin.gz", binary=True, limit=500000)
-with open("keywords.json", "r") as file:
-    keyword_data = json.load(file)
+def word2vec_clustering(keyword_num, threshold):
+    # load Word2Vec model & get the keywords & scores
+    word2vec_model = KeyedVectors.load_word2vec_format("GoogleNews-vectors-negative300.bin.gz", binary=True, limit=500000)
+    with open("keywords.json", "r") as file:
+        keyword_data = json.load(file)
 
 
-keyword_data = {}
-for channel, keywords in keyword_data.items():
-    keyword_data[channel] = keywords[:15]
-data = {
-    "YouTuber": [], #YouTuber
-    "Keywords": [], #keywords & their weights
-}
-for channel, keywords in keyword_data.items():
-    data["youtuber"].append(channel)
-    data["keywords"].append([(k[0], k[1]) for k in keywords])
+    keyword_data = {}
+    for channel, keywords in keyword_data.items():
+        keyword_data[channel] = keywords[:keyword_num]
+    data = {
+        "YouTuber": [], #YouTuber
+        "Keywords": [], #keywords & their weights
+    }
+    for channel, keywords in keyword_data.items():
+        data["youtuber"].append(channel)
+        data["keywords"].append([(k[0], k[1]) for k in keywords])
 
 
-df = pd.DataFrame(data)
-word2vec_graph = nx.Graph()
+    df = pd.DataFrame(data)
+    word2vec_graph = nx.Graph()
 
-# nodes: YouTubers 
-# edges: similarity scores
-for index1, row1 in df.iterrows():
-    word2vec_graph.add_node(row1["youtuber"])
-    for index2, row2 in df.iterrows():
-        if index1 < index2:
-            score = weighted_cosine_similarity(row1["keywords"], row2["keywords"], word2vec_model)
-            if score > 0:
-                word2vec_graph.add_edge(row1["youtuber"], row2["youtuber"], weight=score)
+    # nodes: YouTubers 
+    # edges: similarity scores
+    for index1, row1 in df.iterrows():
+        word2vec_graph.add_node(row1["youtuber"])
+        for index2, row2 in df.iterrows():
+            if index1 < index2:
+                score = weighted_cosine_similarity(row1["keywords"], row2["keywords"], word2vec_model, threshold)
+                if score > 0:
+                    word2vec_graph.add_edge(row1["youtuber"], row2["youtuber"], weight=score)
 
-# plot the graph
-positions = nx.kamada_kawai_layout(word2vec_graph)
-plt.figure(figsize=(10, 10))
-nx.draw(word2vec_graph, positions, node_color='lightblue', with_labels=True, node_size=1500, font_size=12)
-labels = nx.get_edge_attributes(word2vec_graph, 'weight')
-nx.draw_networkx_edge_labels(word2vec_graph, positions, edge_labels=labels)
-nx.write_gexf(word2vec_graph, "graph.gexf")
+    # output the graph
+    positions = nx.kamada_kawai_layout(word2vec_graph)
+    plt.figure(figsize=(10, 10))
+    nx.draw(word2vec_graph, positions, node_color='lightblue', with_labels=True, node_size=1500, font_size=12)
+    labels = nx.get_edge_attributes(word2vec_graph, 'weight')
+    nx.draw_networkx_edge_labels(word2vec_graph, positions, edge_labels=labels)
+    nx.write_gexf(word2vec_graph, "word2vec_graphs/graph_"+keyword_num+"_"+threshold+".gexf")
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello")
+from clustering import word2vec_clustering
+
+
+thresholds = [0.7, 0.8, 0.9, 0.95]
+keywords_nums = [5,10,15,20]
+
+for t in thresholds:
+    for k in keywords_nums:
+        word2vec_clustering(t, k)

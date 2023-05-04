ed04e02a4f8a36cb94d248b1cdb5d1c9d2f82c3d
dq2024 <99848111+dq2024@users.noreply.github.com>
2023-05-03 23:27:39 -0400

    new graphs

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from clustering import word2vec_clustering
 
 
-thresholds = [0.6, 0.7, 0.8, 0.9]
+thresholds = [0.9]
 
-keywords_nums = [5,10,15,20]
+keywords_nums = [5,20]
 
 for t in thresholds:
     for k in keywords_nums:

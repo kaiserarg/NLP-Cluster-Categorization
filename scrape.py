13e01de7cfb32e8e52b1baad046cae3599fc4538
dq2024 <99848111+dq2024@users.noreply.github.com>
2023-05-03 22:07:28 -0400

    new transcript

--- scrape.py
+++ scrape.py
@@ -19,12 +19,12 @@ for i in range(len(youtuberList)):
         for video in videos:
             transcriptList = transcriptAPI.get_transcript(video['videoId'])
             corpus = corpus + formatter.format_transcript(transcriptList, languages=['en', 'en-US']).replace("\n", " ")
+            jsonDict[youtuberList[i][0]] = {"name": youtuberList[i][0], "corpus": corpus}
     except Exception as e:
         print("error is" + youtuberList[i][1])
         continue
     
     print(i)
-    jsonDict[youtuberList[i][0]] = {"name": youtuberList[i][0], "corpus": corpus}
 
 json.dump(jsonDict, f, indent=3)
 f.close()
